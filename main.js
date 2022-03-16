23b3f130f7fbe39fb6a52d8154e8e5d90f7936ad
Sarah Lopez <slopez323@gmail.com>
2022-03-16 17:16:01 -0400

    test again

--- main.js
+++ main.js
@@ -1,44 +1,76 @@
 const screenshotContainer = document.querySelector('#screenshotContainer');
-// const sharebtn = document.querySelector('#share-button');
+const sharebtn = document.querySelector('#share-button');
 
-// sharebtn.addEventListener('click', share);
+sharebtn.addEventListener('click', share);
 
 import html2canvas from 'html2canvas'
 
 
-export default {
-  methods: {
-    share() {
-      // iife here
-      ;(async () => {
-        if (!('share' in navigator)) {
-          return
+// export default {
+//   methods: {
+//     share() {
+//       // iife here
+//       ;(async () => {
+//         if (!('share' in navigator)) {
+//           return
+//         }
+//         // `element` is the HTML element you want to share.
+//         // `backgroundColor` is the desired background color.
+//         const canvas = await html2canvas(screenshotContainer)
+//         canvas.toBlob(async (blob) => {
+//           // Even if you want to share just one file you need to
+//           // send them as an array of files.
+//           const files = [new File([blob], 'image.png', { type: blob.type })]
+//           const shareData = {
+//             text: 'Some text',
+//             title: 'Some title',
+//             files,
+//           }
+//           if (navigator.canShare(shareData)) {
+//             try {
+//               await navigator.share(shareData)
+//             } catch (err) {
+//               if (err.name !== 'AbortError') {
+//                 console.error(err.name, err.message)
+//               }
+//             }
+//           } else {
+//             console.warn('Sharing not supported', shareData)
+//           }
+//         })
+//       })()
+//     },
+//   },
+// }
+
+const share = async() => {
+  if (!('share' in navigator)) {
+    return;
+  }
+  // `element` is the HTML element you want to share.
+  // `backgroundColor` is the desired background color.
+  const canvas = await html2canvas(screenshotContainer, {
+    yellow,
+  });
+  canvas.toBlob(async (blob) => {
+    // Even if you want to share just one file you need to 
+    // send them as an array of files.
+    const files = [new File([blob], 'image.png', { type: blob.type })];
+    const shareData = {
+      text: 'Some text',
+      title: 'Some title',
+      files,
+    };
+    if (navigator.canShare(shareData)) {
+      try {
+        await navigator.share(shareData);
+      } catch (err) {
+        if (err.name !== 'AbortError') {
+          console.error(err.name, err.message);      
         }
-        // `element` is the HTML element you want to share.
-        // `backgroundColor` is the desired background color.
-        const canvas = await html2canvas(screenshotContainer)
-        canvas.toBlob(async (blob) => {
-          // Even if you want to share just one file you need to
-          // send them as an array of files.
-          const files = [new File([blob], 'image.png', { type: blob.type })]
-          const shareData = {
-            text: 'Some text',
-            title: 'Some title',
-            files,
-          }
-          if (navigator.canShare(shareData)) {
-            try {
-              await navigator.share(shareData)
-            } catch (err) {
-              if (err.name !== 'AbortError') {
-                console.error(err.name, err.message)
-              }
-            }
-          } else {
-            console.warn('Sharing not supported', shareData)
-          }
-        })
-      })()
-    },
-  },
-}
+      }
+    } else {
+      console.warn('Sharing not supported', shareData);            
+    }
+  });
+};
